bed8ee16705a68362a5d13619c9aae2edbb1589b
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-07 17:49:40 +0530

    Reading User Input

--- App/App.ts
+++ App/App.ts
@@ -1,4 +1,18 @@
 /* eslint-disable no-unused-vars */
 function onSubmit() {
 	alert("Form was submitted");
+
+	// @ts-ignore
+	const input = document.getElementById("user-input").value;
+
+	if(!input) return alert("JSON was not provided.");
+
+	let json;
+
+	try {
+		json = JSON.parse(input);
+	}
+	catch(err) {
+		alert(`Invalid JSON Provided, ${err.stack.split("\n").slice(0, 2).join(" ")}`);
+	}
 }
